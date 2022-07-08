612fb11b2543e6b1248cacd0e53a34565080c25a
burntcustard <burntcustard@gmail.com>
2022-07-08 20:18:49 +0100

    Make the blue a little darker

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ navigator.mediaDevices.getUserMedia({'video': true})
     a.append(outer);
     outer.append(inner, shuffleButton);
 
-    outer.style.cssText = 'width:3in;background:#7ee;border:2ex solid #7ee;border-radius:2ex;display:grid;gap:4ex;box-shadow:0 0 1ex #1785;margin:3em auto';
+    outer.style.cssText = 'width:3in;background:#7dd;border:2ex solid #7dd;border-radius:2ex;display:grid;gap:4ex;box-shadow:0 0 1ex #1785;margin:3em auto';
     inner.style.cssText = 'height:3in;box-shadow:0 0 1ex #178 inset;filter:drop-shadow(0 0 1ex #178)';
     shuffleButton.style.cssText = 'height:3em;box-shadow:0 0 1ex #1785';
     shuffleButton.innerText = 'Shuffle';
