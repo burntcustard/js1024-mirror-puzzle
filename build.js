404c0caee633574673acb3d96b34937d3c909832
burntcustard <burntcustard@gmail.com>
2022-07-15 01:56:20 +0100

    Improve minifying & build

--- build.js
+++ build.js
@@ -40,8 +40,14 @@ js = js
 
 const minifiedJs = await minifyJs(js, options);
 
-const packed = cmdRegPack(minifiedJs.code, {
-  crushGainFactor: parseFloat(5),
+const code = minifiedJs.code
+  // Replace all double quotes with backticks for consistency
+  .replaceAll('"', '`')
+  // Remove final semicolon
+  .replace(/;$/, '');
+
+const packed = cmdRegPack(code, {
+  crushLengthFactor : parseFloat(1),
 });
 
 const html = readFileSync('src/index.html', 'utf8');
@@ -51,29 +57,32 @@ const inlined = html.replace(
   `<script>${packed}</script>`,
 );
 
+const inlinedNonPacked = html.replace(
+  /<script[^>]*><\/script>/,
+  `<script>${code}</script>`,
+);
+
 const minifiedInlined = minifyHtml(inlined, {
   removeAttributeQuotes: true,
   collapseWhitespace: true,
 });
 
+const minifiedInlinedNonPacked = minifyHtml(inlinedNonPacked, {
+  removeAttributeQuotes: true,
+  collapseWhitespace: true,
+});
+
 const mangled = minifiedInlined
-  // .replaceAll('for(let ', 'for(') // Hoist for() vars to global (very risky) ~4B
-  // .replaceAll('=>{let ', '=>{') // Hoist button onclick lets (very risky) ~8B
-  // .replace('let t=get', 't=get') // Hoist getComputedStyle var (very risky) ~4B
-  // .replace('o=()=>', 'o=v=>') // Assign var to avoid ()
-  // .replace('oncanplay=()', 'oncanplay=w') // Assign var to avoid ()
-  // .replace('onclick=()', 'onclick=x') // Assign var to avoid ()
-  // .replace('onclick=()', 'onclick=y') // Assign var to avoid ()
-  // .replace('()=>Math.random', 'z=>Math.random') // Assign var to avoid ()
   .replace('<!DOCTYPE html><html>', '') // Remove doctype & HTML opening tags
   .replace(';</script>', '</script>') // Remove final semicolon
   .replace('<head>', '') // Remove head opening tag
   .replace('</head>', '') // Remove head closing tag
+  .replace('"initial-scale=1"', 'initial-scale=1') // Remove initial-scale quotes
   .replace('<body>', '') // Remove body opening tag
   .replace('</body></html>', ''); // Remove closing tags
 
 console.log(`Mangled: ${mangled.length}B`);
 
+writeFileSync('index.nonpacked.html', minifiedInlinedNonPacked);
 writeFileSync('index.watch.html', minifiedInlined);
-
 writeFileSync('index.html', mangled);
